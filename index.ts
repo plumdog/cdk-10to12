bcad13b3ca48269f7c2695badd1136a51de7d3f5
Andrew Plummer <plummer574@gmail.com>
2022-02-28 23:09:10 +0000

    S3 notifications test and fix (#3)

--- index.ts
+++ index.ts
@@ -6,6 +6,12 @@ const fixConstructIfRequired = (construct: cdk.IConstruct): void => {
         if (construct.runtime === 'nodejs10.x') {
             construct.addPropertyOverride('Runtime', lambda.Runtime.NODEJS_12_X.toString());
         }
+    } else if (construct instanceof cdk.CfnResource && construct.cfnResourceType === 'AWS::Lambda::Function') {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (construct.cfnProperties.Runtime === 'nodejs10.x') {
+            construct.addPropertyOverride('Runtime', lambda.Runtime.NODEJS_12_X.toString());
+        }
     }
 };
 
